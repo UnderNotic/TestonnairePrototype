851331d1162f72e46a8a6a1edb3aaa01fa2876c2
unknown <undernotic@gmail.com>
2015-01-30 00:44:34 +0100

    Migrated to newest version of SOCKET.IO

--- public/app.js
+++ public/app.js
@@ -38,76 +38,8 @@ jQuery(function($){
          */
         onConnected : function() {
             // Cache a copy of the client's socket.IO session ID on the App
-            App.mySocketId = IO.socket.socket.sessionid;
+            App.mySocketId = IO.socket.io.engine.id; 
             // console.log(data.message);
-        },
-
-        /**
-         * A new game has been created and a random game ID has been generated.
-         * @param data {{ gameId: int, mySocketId: * }}
-         */
-        onNewGameCreated : function(data) {
-            App.Host.gameInit(data);
-        },
-
-        /**
-         * A player has successfully joined the game.
-         * @param data {{playerName: string, gameId: int, mySocketId: int}}
-         */
-        playerJoinedRoom : function(data) {
-            // When a player joins a room, do the updateWaitingScreen funciton.
-            // There are two versions of this function: one for the 'host' and
-            // another for the 'player'.
-            //
-            // So on the 'host' browser window, the App.Host.updateWiatingScreen function is called.
-            // And on the player's browser, App.Player.updateWaitingScreen is called.
-            App[App.myRole].updateWaitingScreen(data);
-        },
-
-        /**
-         * Both players have joined the game.
-         * @param data
-         */
-        beginNewGame : function(data) {
-            App[App.myRole].gameCountdown(data);
-        },
-
-        /**
-         * A new set of words for the round is returned from the server.
-         * @param data
-         */
-        onNewWordData : function(data) {
-            // Update the current round
-            App.currentRound = data.round;
-
-            // Change the word for the Host and Player
-            App[App.myRole].newWord(data);
-        },
-
-        /**
-         * A player answered. If this is the host, check the answer.
-         * @param data
-         */
-        hostCheckAnswer : function(data) {
-            if(App.myRole === 'Host') {
-                App.Host.checkAnswer(data);
-            }
-        },
-
-        /**
-         * Let everyone know the game has ended.
-         * @param data
-         */
-        gameOver : function(data) {
-            App[App.myRole].endGame(data);
-        },
-
-        /**
-         * An error has occurred.
-         * @param data
-         */
-        error : function(data) {
-            alert(data.message);
         }
 
     };
